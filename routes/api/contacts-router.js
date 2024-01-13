1e0a9b38f72113f1fe14b7bd6a3a424241eb4831
Angelica <lisoveckaya1997@gmail.com>
2024-01-13 13:57:10 +0200

    upd

--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -3,19 +3,7 @@ import contactsOperations from "../../models/contacts.js";
 
 import HttpError from "../../helpers/HtttpError.js";
 
-import Joi from "joi";
-
-const contactSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().email().required().messages({
-    "string.email": "Incorrect E-Mail Address",
-    "any.required": "Email is required. Please provide an email address.",
-  }),
-  phone: Joi.string()
-    .pattern(/^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/)
-    .required(),
-});
-
+import contactSchema from "../../schemas/contacts-schema.js";
 
 const router = express.Router();
 
@@ -77,7 +65,7 @@ router.put("/:contactId", async (req, res, next) => {
   try {
     const { error } = contactSchema.validate(req.body);
     if (error) {
-      throw HttpError(400, "missing fields");
+      throw HttpError(400, error.message);
     }
 
     const { contactId } = req.params;
