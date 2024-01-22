f67e29b138514873d3ca121462f6478542055aa6
Angelica <lisoveckaya1997@gmail.com>
2024-01-22 21:35:59 +0200

    test

--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -4,13 +4,16 @@ import validateBody from "../../decorators/validateBody.js";
 import usersSchemas from "../../schemas/users-schema.js";
 import authController from "../../controllers/auth-controller.js";
 import authenticate from "../../middlewars/authenticate.js";
+import upload from "../../middlewars/upload.js";
 
 authRouter.post("/register", validateBody(usersSchemas.userSignupSchema), authController.signup);
 
 authRouter.post("/login", validateBody(usersSchemas.userSigninSchema), authController.signin);
 authRouter.get("/current", authenticate, authController.getCurrent);
 authRouter.post("/logout", authenticate, authController.logout);
-authRouter.patch("/", authenticate, authController.changeSubscription)
+authRouter.patch("/", authenticate, authController.changeSubscription);
+authRouter.patch("/avatars", authenticate, upload.single("avatar"), authController.updateAvatar);
+
 
 
 export default authRouter;
